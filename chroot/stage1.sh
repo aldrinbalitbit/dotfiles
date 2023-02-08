7a3f9ccaf10d79756ef408320732538537b94f9c
aldrinbalitbit <aldrinbalitbit1127@gmail.com>
2023-02-08 09:59:17 +0800

    Use 6 on fixing ncurses

--- chroot/stage1.sh
+++ chroot/stage1.sh
@@ -17,7 +17,6 @@ apt-get -qqy install gcc make g++ ca-certificates wget bash
 echo "Compiling ncurses..."
 wget -qO- https://github.com/ThomasDickey/ncurses-snapshots/archive/refs/heads/master.tar.gz | tar -zxf- -C /sources/
 mv /sources/ncurses-snapshots-master /sources/ncurses && cd /sources/ncurses
-_ncurses_version=6.4
 ./configure --prefix=/usr \
 	    --libdir=/usr/lib/x86_64-linux-gnu \
 	    --mandir=/usr/share/man \
@@ -38,10 +37,10 @@ for lib in ncurses ncurses++ form panel menu; do
 	ln -sv ${lib}w.pc "/usr/lib/x86_64-linux-gnu/pkgconfig/${lib}.pc"
 done
 printf 'INPUT(-lncursesw)\n' > "/usr/lib/x86_64-linux-gnu/libcursesw.so"
-ln -sv libncurses.so "/lib/x86_64-linux-gnu/libcurses.so"
+ln -sv libncurses.so "/usr/lib/x86_64-linux-gnu/libcurses.so"
 for lib in tic tinfo; do
-	printf "INPUT(libncursesw.so.%s)\n" "${_ncurses_version:0:1}" > "/usr/lib/x86_64-linux-gnu/lib${lib}.so"
-	ln -sv libncursesw.so.${_ncurses_version:0:1} "/usr/lib/x86_64-linux-gnu/lib${lib}.so.${_ncurses_version:0:1}"
+	printf "INPUT(libncursesw.so.6)\n" > "/usr/lib/x86_64-linux-gnu/lib${lib}.so"
+	ln -sv libncursesw.so.6 "/usr/lib/x86_64-linux-gnu/lib${lib}.so.6"
 	ln -sv ncursesw.pc "/usr/lib/x86_64-linux-gnu/pkgconfig/${lib}.pc"
 done
 # Compile readline
